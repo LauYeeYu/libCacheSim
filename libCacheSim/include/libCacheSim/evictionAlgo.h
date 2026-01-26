fb1bf9acb00cc52ca83cb2349faa0ed2cc1a69d4
Yiyu Liu <liuyiyu2003@gmail.com>
2026-01-26 17:49:35 -0500

    feat: LHD Compute

--- libCacheSim/include/libCacheSim/evictionAlgo.h
+++ libCacheSim/include/libCacheSim/evictionAlgo.h
@@ -107,6 +107,9 @@ cache_t *LFUDA_init(const common_cache_params_t ccache_params,
 cache_t *LHD_init(const common_cache_params_t ccache_params,
                   const char *cache_specific_params);
 
+cache_t *LHD_compute_init(const common_cache_params_t ccache_params,
+                         const char *cache_specific_params);
+
 cache_t *LIRS_init(const common_cache_params_t ccache_params,
                    const char *cache_specific_params);
 
